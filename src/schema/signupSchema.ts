8af2663ab15c67184c6473c0b7b3003826bef47c
rishabhpandey106 <rishabhpandey230@gmail.com>
2024-04-17 21:19:26 +0530

    added verification api

--- src/schema/signupSchema.ts
+++ src/schema/signupSchema.ts
@@ -1,7 +1,9 @@
 import z from 'zod'
 
+export const UsernameSchema = z.string().min(4 , "Username must be atleast 4 characters").max(8 , "Username must be atmost 8 characters").regex(/^[a-zA-Z0-9_]+$/, 'Username must not contain special characters')
+
 export const signupSchema = z.object({
-    username: z.string().min(4 , "Username must be atleast 4 characters").max(8 , "Username must be atmost 8 characters").regex(/^[a-zA-Z0-9_]+$/, 'Username must not contain special characters'),
+    username: UsernameSchema,
     email: z.string().email({message: "Invalid email address"}),
     password: z.string().min(6,"Password must be atleast 6 characters")
 })
