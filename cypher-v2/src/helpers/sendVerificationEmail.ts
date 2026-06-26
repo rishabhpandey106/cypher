577a2038d6763e233fb6407e09732890dbff33f9
rishabhpandey106 <rishabhpandey230@gmail.com>
2026-06-26 18:54:10 +0530

    chore: email domain

--- cypher-v2/src/helpers/sendVerificationEmail.ts
+++ cypher-v2/src/helpers/sendVerificationEmail.ts
@@ -10,7 +10,7 @@ export async function sendVerificationEmail(
     try {
 
         await resend.emails.send({
-            from: 'Cypher <cypher@contact.itsrishabh.tech>',
+            from: 'Cypher <noreply@contact.itsrishabh.tech>',
             to: email,
             subject: 'Verification Code',
             react: VerificationEmail({username, otp: verifyCode}),
